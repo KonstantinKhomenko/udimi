271143a5fbcc2f733dc24208a47076dfd63869a1
Konstantin Khomenko <k.khomenko@upstars.com>
2023-09-21 00:59:00 +0300

    projects page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,67 @@
 <template>
-  <Tutorial />
+  <div>
+    <ProjectItem
+      v-for="(project, index) in userProjects"
+      :key="index"
+      class="project-item"
+      :project="project"
+      @click.native="onClickProject(project.id)"
+    />
+
+    <project-item-modal
+      v-if="isShownModal"
+      :selected-id="selectedId"
+      @closeModal="onCloseModal"
+      @projectUpdated="fetchProjects"
+    />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import ProjectItemModal from '~/components/ProjectItemModal.vue'
+import ProjectItem from '~/components/ProjectItem.vue'
 
-export default defineComponent({
-  name: 'IndexPage',
-})
+export default {
+  name: 'ProjectsPage',
+
+  components: {
+    ProjectItemModal,
+    ProjectItem,
+  },
+
+  middleware: ['auth'],
+
+  data: () => ({
+    selectedId: null,
+    isShownModal: false,
+  }),
+
+  async fetch({ store }) {
+    await store.dispatch('projects/fetchProjects')
+  },
+
+  computed: {
+    ...mapGetters('projects', ['userProjects']),
+  },
+
+  methods: {
+    ...mapActions('projects', ['fetchProjects']),
+
+    onClickProject(id) {
+      this.selectedId = id
+      this.isShownModal = true
+    },
+
+    onCloseModal() {
+      this.isShownModal = false
+    },
+  },
+}
 </script>
+
+<style scoped>
+.project-item:not(:last-child) {
+  margin-bottom: 0.5rem;
+}
+</style>
