6be047706c2db62310a9c620a001cd6b66b111b4
Konstantin Khomenko <k.khomenko@upstars.com>
2023-09-21 00:52:58 +0300

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-navbar />
+
+    <main>
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+import AppNavbar from '~/components/AppNavbar.vue'
+
+export default {
+  components: {
+    AppNavbar,
+  },
+}
+</script>
